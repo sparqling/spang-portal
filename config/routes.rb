88e117439a5f91a4139966c19da6b85d46528bba
Shota Matsumoto <shota.matsumoto@lifematics.co.jp>
2020-03-12 14:06:17 +0900

    fix named route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   namespace :api do
     get 'search', to: 'library#search_template', as: 'library_search_template'
     resources :library, only: [:index, :show]
-    get 'library/:library_id/:template_id', to: 'library#query_template', as: 'library_show_template'
+    get 'library/:library_id/:template_id', to: 'library#query_template', as: 'library_query_template'
   end
 end
