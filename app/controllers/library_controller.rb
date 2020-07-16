0eaabc8335aa4e3520539dbe38d45c0f8318df2a
Shota Matsumoto <shota.matsumoto@lifematics.co.jp>
2020-07-16 17:33:07 +0900

    Content negotiation in LibraryController

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -9,6 +9,11 @@ class LibraryController < ApplicationController
 
   def show_template
     @template = Library.new(params[:library_id]).templates.select{ |template| template.name == params[:template_id]}.first
+    respond_to do |format|
+      format.all { render action: 'show_template.html.haml', content_type: "text/html" }
+      format.text { render plain: @template.query }
+      format.json { render json: { query: @template.query } }
+    end
   end
 
   def search_template
